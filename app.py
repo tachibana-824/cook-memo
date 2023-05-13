991259b5c22480dd30d882d5092eebd5e9ea45b6
unknown <yama.rei.824@gmail.com>
2023-05-13 09:54:24 +0900

    1st commit

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import streamlit as st
 # with open('secret.json') as f:
 #     secret = json.load(f)
 
+
+#再生リストから動画IDを取得する関数
 def getIdListFromPlaylist(id,youtube,keyword):
 
     nextPageToken = 'start'
+    #動画IDを格納する変数response
     response = []
 
     while(nextPageToken is not None):
@@ -32,6 +35,7 @@ def getIdListFromPlaylist(id,youtube,keyword):
             nextPageToken = None
         
         for item in search_response['items']:
+            #キーワードが概要欄にあるかを判別
             if keyword in item['snippet']['description']:
                 response.append(item['snippet']['resourceId']['videoId'])
             
@@ -51,7 +55,7 @@ st.title('cook memo')
 st.sidebar.write('キーワード入力')
 keyword = st.sidebar.text_input('キーワードを入力してください','例 :にんじん')
 st.sidebar.write('再生リスト入力')
-id = st.sidebar.text_input('再生リストidを入力してください',)
+id = st.sidebar.text_input('再生リストidを入力してください', 'FLT-fSokxQnIP7MOzwXePchw')
 
 
 st.write('キーワード :',keyword)
@@ -61,15 +65,27 @@ st.write('再生リスト :',id)
 
 
 if st.button('動画表示'):
-    video_ids = getIdListFromPlaylist(id=id,youtube=youtube,keyword=keyword)
-    for video_id in video_ids:
-        
-        url = f'https://www.youtube.com/watch?v={video_id}'
-        video_field = st.empty()
+    if len(keyword) == 0:
+        st.write('キーワードを入力してください')
+    elif len(id) == 0:
+        st.write('再生リストidを入力してください')
+    else:
+        try:
+            video_ids = getIdListFromPlaylist(id=id,youtube=youtube,keyword=keyword)    
+            if len(video_ids) == 0:
+                st.write('動画が見つかりません')
+            else:                    
+                for video_id in video_ids:
+                    #動画IDから動画urlを作成
+                    url = f'https://www.youtube.com/watch?v={video_id}'
+                    video_field = st.empty()
 
-        if len(video_id)>0:
-            try:
-                video_field.video(url)
-            except:
-                st.error('リンクが間違っています。')
+                    if len(video_id)>0:
+                        try:
+                            #動画を表示する
+                            video_field.video(url)
+                        except:
+                            st.error('リンクが間違っています。')
+        except:
+            st.write('再生リストidが間違っています')                    
 
